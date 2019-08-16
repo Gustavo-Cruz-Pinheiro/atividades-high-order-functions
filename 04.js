a9ce5c963fae098344d6b0461aad29626828fbb0
Gustavo-Cruz-Pinheiro <54116994+Gustavo-Cruz-Pinheiro@users.noreply.github.com>
2019-08-15 23:01:59 -0400

    Update 04.js

--- 04.js
+++ 04.js
@@ -3,7 +3,7 @@ const data = require("./data.js");
 data.forEach(usuario => (usuario.age = usuario.age * 2));
 
 const usuariosFiltradosMax50Anos = data.filter(usuario =>
-  usuario.age < 50 ? true : false
+  usuario.age <= 50 ? true : false
 );
 
 console.table(usuariosFiltradosMax50Anos);
